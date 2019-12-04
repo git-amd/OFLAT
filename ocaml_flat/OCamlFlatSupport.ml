e5a0e98e782289cd4f1cf45932e9749c9d625428
U-DESKTOP-NK4UQP8\Joao <jmg.goncalves@campus.fct.unl.pt>
2019-12-04 13:37:03 +0000

    updated support file

--- ocaml_flat/OCamlFlatSupport.ml
+++ ocaml_flat/OCamlFlatSupport.ml
@@ -1,4 +1,17 @@
-(* OCamlFlatSupport.ml - AMD/2019 *)
+(** OCaml-Flat - An OCaml library for FLAT concepts
+ *
+ * Support code for the library
+ *
+ * @version v 0.3
+ *
+ * @author Artur Miguel Dias <amd@fct.unl.pt>  [main author]
+ * @author Joao Goncalves <jmg.goncalves@campus.fct.unl.pt>
+ * @author Rita Macedo <rp.macedo@campus.fct.unl.pt>
+ * @author Antonio Ravara <aravara@fct.unl.pt>
+ *
+ * LICENCE - As now this is a private project, but later it
+ *            will be open-source.
+*)
 
 open Yojson.Basic.Util
 
@@ -15,7 +28,7 @@ module Error = struct
 	let start () =	(* setup a log of errors *)
 		let r = ref [] in
 			err := r;
-			r        (* returns a new error log *)
+			r		(* returns a new error log *)
 
 	let stop () = (* disables logging *)
 		sink := [];
@@ -58,38 +71,49 @@ module Util =  struct
 		println "----------" ; println str
 
 	let printAlphabet (alf:char list) =
-	    List.iter (fun x -> print_char x; print_string ", ") alf;
+		List.iter (fun x -> print_char x; print_string ", ") alf;
 		println " "
-		
+
 	let printStates (st:string list) =
-	    List.iter (fun x -> print_string x; print_string ", ") st;
+		List.iter (fun x -> print_string x; print_string ", ") st;
 		println " "
-		
-	let printTransition (a:string) (b:char) (c:string) = 
+
+	let printTransition (a:string) (b:char) (c:string) =
 		print_string "("; print_string a; print_string ", ";
 		print_char b; print_string ", ";
 		print_string c; println ")"
 
 	let printWord (w:char list) =
-	    print_char '\'' ; List.iter (fun c -> print_char c ) w; println "'"
+		print_char '\'' ; List.iter (fun c -> print_char c ) w; println "'"
 
 	let printWordList (l:char list list) =
-	    List.iter printWord l
+		List.iter printWord l
 
-	let addAll symb = List.map (fun l -> symb::l)
-	
-	let distrib2 f (a,b) = f a b 
+	let listFromString s = List.init (String.length s) (String.get s)
 
-    let indexOf e l =
-        let rec index e l n =
-            match l with
-                [] -> -1
-                |x::xs -> if e = x then n else index e xs (n+1)
-        in
-        index e l 0
+	let stringFromList l =
+		let buf = Buffer.create 16 in
+		let () = List.iter (Buffer.add_char buf) l in
+			Buffer.contents buf
 
+	let addAll symb s = List.map (fun l -> symb::l) s
 
+	let concatAll w s = List.map (fun l -> w@l) s
 
+	let distrib2 f (a,b) = f a b
+
+	let indexOf e l =
+		let rec index e l n =
+			match l with
+				[] -> -1
+				|x::xs -> if e = x then n else index e xs (n+1)
+		in
+		index e l 0
+
+	let rec fixedPoint (f: 'a -> 'a) (x: 'a): 'a =
+		let next = f x in
+			if x = next then x
+			else fixedPoint f next
 end
 
 module UtilTests =
@@ -114,36 +138,145 @@ end
 
 (* --- Set --- *)
 
-module Set = struct
+module Set : (* regular set *)
+sig
+	(*type 'a t = 'a list*)
+	type 'a t
+	val make : 'a list -> 'a t
+	val toList : 'a t -> 'a list
+	val empty : 'a t
+	val size : 'a t -> int
+	val belongs : 'a -> 'a t -> bool
+	val union : 'a t -> 'a t -> 'a t
+	val add : 'a -> 'a t -> 'a t
+	val inter : 'a t -> 'a t -> 'a t
+	val diff : 'a t -> 'a t -> 'a t
+	val subset : 'a t -> 'a t -> bool
+	val map : ('a -> 'b) -> 'a t -> 'b t
+	val filter : ('a -> bool) -> 'a t -> 'a t
+	val for_all : ('a -> bool) -> 'a t -> bool
+	val exists : ('a -> bool) -> 'a t -> bool
+	val flatten : 'a t t -> 'a t
+	val flatMap : ('a -> 'b t) -> 'a t -> 'b t
+	val iter : ('a -> unit) -> 'a t -> unit
+	val partition : ('a -> bool) -> 'a t -> 'a t * 'a t
+	val combinations : 'a t -> 'b t -> ('a * 'b) t
+	val star : 'a list t -> int -> 'a list t
+	val allDistinct : ('a -> 'b) -> 'a t -> bool
+	val hasDuplicates : 'a t -> bool
+	val validate : 'a list -> string -> 'a t
+	val test: unit -> int list list
+  end
+= struct
 	type 'a t = 'a list
-	let empty: 'a = []
-	let size (s: 'a list): int = List.length s
+	let make (l: 'a list): 'a t = List.sort_uniq compare l
+	let toList (s: 'a t): 'a list = s
+
+	let empty: 'a t = []
+	let size (s: 'a t): int = List.length s
 	let belongs (v: 'a) (s: 'a t): bool = List.mem v s
-	let clear (l: 'a list): 'a t = List.sort_uniq compare l	 
-	let make (l: 'a list): 'a t = clear l	 
-	let union (s1: 'a t) (s2: 'a t): 'a t = clear (s1 @ s2)
-	let add (v: 'a) (s: 'a t): 'a t = clear (v :: s)
+	let union (s1: 'a t) (s2: 'a t): 'a t = make (s1 @ s2)
+	let add (v: 'a) (s: 'a t): 'a t = make (v :: s)
 	let inter (s1: 'a t) (s2: 'a t): 'a t = List.filter (fun x -> belongs x s2) s1
 	let diff (s1: 'a t) (s2: 'a t): 'a t = List.filter (fun x -> not (belongs x s2)) s1
 	let subset (s1: 'a t) (s2: 'a t): bool = List.for_all (fun x -> belongs x s2) s1
 
-	
-	let map f s = clear (List.map f s)
+	let map f s = make (List.map f s)
 	let filter f s = List.filter f s
 	let for_all f s = List.for_all f s
 	let exists f s = List.exists f s
-	let flatten ss = clear (List.flatten ss)
-	let flatMap f s = clear (List.flatten (List.map f s))
+	let flatten ss = make (List.flatten ss)
+	let flatMap f s = flatten (List.map f s)
 	let iter f s = List.iter f s
-	let partition f s = let (a, b) = List.partition f s in (clear a, clear b)
-	let combinations aSet bSet = flatMap (fun x -> List.map ( fun y -> (x,y) ) bSet) aSet
-	let allDistinct f s = size s = size (map f s)
+	let partition f s = let (a, b) = List.partition f s in (a, b)  (* <- already ordered *)
+	let combinations (s1: 'a t) (s2: 'b t): ('a * 'b) t =
+		flatMap (fun x -> List.map (fun y -> (x,y)) s2) s1  (* <- already ordered *)
+	let starOne (s: 'a list t) (n: int) (l:'a list): 'a list t = (* private auxiliary *)
+		let z = n - (List.length l) in
+		let sel = filter (fun k -> List.length k <= z) s in
+			map (fun k -> k @ l) sel
+	let star (s: 'a list t) (n: int): 'a list t =
+		Util.fixedPoint (fun x -> union x (flatMap (starOne x n) s)) [[]]
 
-	let hasDuplicates (s: 'a list): bool = size s <> size (clear s)
+	let allDistinct f s = size s = size (map f s)
+	let hasDuplicates (s: 'a t): bool = size s <> size (make s)
 	let validate (l: 'a list) (culprit: string): 'a t =
 		if hasDuplicates l
 			then Error.error culprit "Repetitions in set" empty
-			else clear l
+			else make l
+
+	let test (): int list list =  (* Set.test ();; *)
+		toList (star (make[ [1]; [2;3]]) 4)
+end
+
+module UPSet : (* unordered pair set *)
+  sig
+	type 'a t
+	val make : ('a * 'a) list -> 'a t
+	val toList : 'a t -> ('a * 'a) list
+	val empty : 'a t
+	val size : 'a t -> int
+	val belongs : 'a * 'a -> 'a t -> bool
+	val union : 'a t -> 'a t -> 'a t
+	val add : 'a * 'a -> 'a t -> 'a t
+	val inter : 'a t -> 'a t -> 'a t
+	val diff : 'a t -> 'a t -> 'a t
+	val subset : 'a t -> 'a t -> bool
+	val map : ('a * 'a -> 'b * 'b) -> 'a t -> 'b t
+	val filter : ('a * 'a -> bool) -> 'a t -> ('a * 'a) Set.t
+	val for_all : ('a * 'a -> bool) -> 'a t -> bool
+	val exists : ('a * 'a -> bool) -> 'a t -> bool
+	val flatten : 'a t t -> 'a t
+	val flatMap : ('a -> 'b t) -> 'a t -> 'b t
+	val iter : ('a * 'a -> unit) -> 'a t -> unit
+	val partition : ('a * 'a -> bool) -> 'a t -> ('a * 'a) Set.t * ('a * 'a) Set.t
+	val combinations : 'a t -> 'b t -> ('a * 'b) t
+	val star : 'a list t -> int -> 'a list t
+	val allDistinct : ('a * 'a -> 'b) -> 'a t -> bool
+	val hasDuplicates : 'a t -> bool
+	val validate : ('a * 'a) list -> string -> 'a t
+	val test: unit -> (int * int) list
+  end
+ =
+ struct
+	type 'a t = ('a*'a) Set.t
+
+	(* invariant: a < b for all pairs (a,b) *)
+	let ord (a,b) = if a < b then (a, b)        (* keep *)
+					else if a > b  then (b, a)  (* swap *)
+					else failwith "UPSet.ord"   (* error *)
+
+	let make (l: ('a*'a) list): 'a t =
+		let l1 = List.filter (fun (a,b) -> a <> b) l in
+		let l2 = List.map ord l1 in
+			Set.make l2
+	let toList (s: 'a t): ('a*'a) list = Set.toList s
+
+	let empty: 'a t = Set.empty
+	let size (s: 'a t): int = Set.size s
+	let belongs (v: 'a*'a) (s: 'a t): bool = Set.belongs (ord v) s
+	let union (s1: 'a t) (s2: 'a t): 'a t = Set.union s1 s2
+	let add (v: 'a*'a) (s: 'a t): 'a t = Set.add (ord v) s
+	let inter (s1: 'a t) (s2: 'a t): 'a t = Set.inter s1 s2
+	let diff (s1: 'a t) (s2: 'a t): 'a t = Set.diff s1 s2
+	let subset (s1: 'a t) (s2: 'a t): bool = Set.subset s1 s2
+
+	let map f (s: 'a t) = make (Set.toList (Set.map f s))
+	let filter f (s: 'a t) = Set.filter f s
+	let for_all f (s: 'a t) = Set.for_all f s
+	let exists f (s: 'a t) = Set.exists f s
+	let flatten (ss: 'a t t) = failwith "UPSet.flatten"
+	let flatMap f (s: 'a t) = failwith "UPSet.flatMap"
+	let iter f (s: 'a t) = Set.iter f s
+	let partition f (s: 'a t) = Set.partition f s
+	let combinations (s1: 'a t) (s2: 'b t): ('a * 'b) t = failwith "UPSet.combinations"
+	let star (s: 'a list t) (n: int): 'a list t = failwith "UPSet.star"
+
+	let allDistinct f (s: 'a t) = Set.allDistinct f s
+	let hasDuplicates (s: 'a t): bool = Set.hasDuplicates s
+	let validate (l: ('a*'a) list) (culprit: string): 'a t = failwith "UPSet.validate"
+	let test () =  (* UPSet.test ();; *)
+		toList (make [(1,1);(1,2);(2,2);(3,2);(3,2);(2,3)])
 end
 
 
@@ -153,7 +286,7 @@ module JSon = struct
 	type t = Yojson.Basic.t
 
 	type 'r alternatives =
-		  JSon of t
+		| JSon of t
 		| Text of string
 		| File of string
 		| Representation of 'r
@@ -174,32 +307,32 @@ module JSon = struct
 
 	let from_alternatives alt =
 		match alt with
-			  JSon j -> j
+			| JSon j -> j
 			| Text str -> from_string str
 			| File str -> from_file str
 			| Representation r -> `Null
 
 	let get_representation alt =
 		match alt with
-			  JSon j -> failwith "get_representation"
+			| JSon j -> failwith "get_representation"
 			| Text str -> failwith "get_representation"
 			| File str -> failwith "get_representation"
 			| Representation r -> r
 
 	let field_string (j: t) (field: string) =
 		match j |> member field with
-			  `Null -> error field "Missing field" "#"
+			| `Null -> error field "Missing field" "#"
 			| `String s -> s
 			| _ -> error field "Expected string" "#"
 
 	let as_string (j: t) (field: string) =
 		match j with
-			  `String s -> s
+			| `String s -> s
 			| _ -> error field "Expected string" "#"
 
 	let field_string_list (j: t) (field: string) =
 		match j |> member field with
-			  `Null -> error field "Missing field" []
+			| `Null -> error field "Missing field" []
 			| `List l -> List.map (fun j -> as_string j field) l
 			| _ -> error field "Expected string list" []
 
@@ -208,12 +341,12 @@ module JSon = struct
 
 	let as_char (j: t) (field: string) =
 		match j with
-			  `String s when String.length s = 1 -> String.get s 0
+			| `String s when String.length s = 1 -> String.get s 0
 			| _ -> error field "Expected char" '#'
 
 	let field_char_list (j: t) (field: string) =
 		match j |> member field with
-			  `Null -> error field "Missing field" []
+			| `Null -> error field "Missing field" []
 			| `List l -> List.map (fun j -> as_char j field) l
 			| _ -> error field "Expected char list" []
 
@@ -222,13 +355,13 @@ module JSon = struct
 
 	let as_string_char_string (j: t) (field: string) =
 		match j with
-			  `List [a; b; c] -> (as_string a field, as_char b field, as_string c field)
+			| `List [a; b; c] -> (as_string a field, as_char b field, as_string c field)
 			| _ -> error field "Malformed triple" ("#",'#',"#")
 
 	let field_triples_list (j: t) (field: string) =
 		match j |> member field with
-		  `List l -> List.map (fun j -> as_string_char_string j field) l
-		| _ -> []
+			| `List l -> List.map (fun j -> as_string_char_string j field) l
+			| _ -> []
 
 	let field_triples_set (j: t) (field: string) =
 		Set.validate (field_triples_list j field) field
@@ -339,10 +472,10 @@ module RegExpSyntax = struct
 			| ')' -> skip(); Empty
 			| _ ->
 				let e = parse_exp () in (
-		        	match curr() with
-		         		| ')' -> skip(); e
-		                | _ -> failwith "Right-parenthesis expected\n"
-			 	)
+					match curr() with
+						| ')' -> skip(); e
+						| _ -> failwith "Right-parenthesis expected\n"
+				)
 
 	let parse s =
 		inputString := s;
@@ -391,4 +524,3 @@ module RegExpSyntaxTests = struct
 			test1 ()
 	)
 end
-
